620edc493053ee645aa19f8d2c124575d8ea78d2
chachinn <140357822+chachinn@users.noreply.github.com>
2026-08-13 21:50:32 +0000

    Fuwa v86: easier Daily Life writing and stability polish

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = "fuwa-shell-v85";
+const CACHE_NAME = "fuwa-shell-v86";
 const RELEASE_MARKER_CACHE = "fuwa-release-state";
 const RELEASE_MARKER_REQUEST = "./__fuwa_release_marker__";
-const RELEASE_KEY = "fuwa-v1.1.0-2026-08-13";
+const RELEASE_KEY = "fuwa-v1.1.1-2026-08-14";
 
 const CORE_ASSETS = [
   "./",
